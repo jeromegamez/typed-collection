74b19af0ac65b352fe50ef9dd0eea29cd2f4b38f
Jérôme Gamez <jerome@gamez.name>
2017-11-07 21:31:40 +0100

    Simplify tests

--- tests/TypedCollectionTest.php
+++ tests/TypedCollectionTest.php
@@ -2,43 +2,51 @@
 
 namespace Gamez\Illuminate\Support\Tests;
 
+use DateTime;
+use DateTimeInterface;
 use Gamez\Illuminate\Support\TypedCollection;
 use Illuminate\Support\Collection;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class TypedCollectionTest extends TestCase
 {
+    /**
+     * @var TypedCollection
+     */
+    private $collection;
+
+    protected function setUp()
+    {
+        $this->collection = new class extends TypedCollection {
+            protected static $allowedTypes = [DateTimeInterface::class];
+        };
+    }
+
     /** @test */
     public function it_only_accepts_items_of_a_given_type()
     {
-        $items = [new \DateTime(), new \DateTimeImmutable()];
+        $this->collection->push(new DateTime());
+        $this->collection->prepend(new DateTime());
+        $this->collection->put(999, new DateTime());
 
-        $collection = new class($items) extends TypedCollection {
-            protected static $allowedTypes = [\DateTimeInterface::class];
-        };
-
-        $collection->push(new \DateTime());
-        $collection->prepend(new \DateTime());
-        $collection->put(999, new \DateTime());
-
-        $this->assertCount(5, $collection);
+        $this->assertCount(3, $this->collection);
     }
 
     /** @test */
     public function it_does_not_accept_an_unsupported_type()
     {
-        $items = [new \stdClass()];
+        $this->expectException(InvalidArgumentException::class);
 
-        $this->expectException(\InvalidArgumentException::class);
-
-        new class($items) extends TypedCollection {
-            protected static $allowedTypes = [\DateTimeInterface::class];
-        };
+        $this->collection->push(new \stdClass());
     }
 
     /** @test */
     public function it_can_be_untyped()
     {
-        $this->assertNotInstanceOf(TypedCollection::class, (new TypedCollection())->untype());
+        $untyped = $this->collection->untype();
+
+        $this->assertInstanceOf(Collection::class, $untyped);
+        $this->assertNotInstanceOf(TypedCollection::class, $untyped);
     }
 }
