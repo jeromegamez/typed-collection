e9c7e2fab5f3f5e21afb5559ce1a4073016ad3b6
Steve Porter <steve@steveporter.io>
2024-03-15 10:34:14 +0000

    feat: Add typedCollect helper function and related tests

---
+++ src/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+use Gamez\Illuminate\Support\TypedCollection;
+
+if (!function_exists('typedCollect')) {
+    /**
+     * Create an anonymous typed collection
+     *
+     * @template TKey of array-key
+     * @template TValue
+     *
+     * @param  \Illuminate\Contracts\Support\Arrayable<TKey, TValue>|iterable<TKey, TValue>|null  $value
+     * @param string|array $type
+     * @return \Illuminate\Support\Collection<TKey, TValue>
+     */
+    function typedCollect($value = [], string|array $types = [])
+    {
+        $types = is_array($types) ? $types : [$types];
+
+        $collection = new class extends TypedCollection
+        {
+        };
+
+        $reflectionClass = new ReflectionClass($collection);
+        $reflectionProperty = $reflectionClass->getProperty('allowedTypes');
+        $reflectionProperty->setValue($collection, $types);
+
+        return $collection::make($value);
+    }
+}
