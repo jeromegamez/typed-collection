6b19bd45fd56263c1ea6673ee8f9f79439c02fe6
Jérôme Gamez <jerome@gamez.name>
2024-03-17 09:57:58 +0100

    Fix tyop (`$type` to `$types`)

--- src/helpers.php
+++ src/helpers.php
@@ -9,17 +9,15 @@ if (!function_exists('typedCollect')) {
      * @template TKey of array-key
      * @template TValue
      *
-     * @param  \Illuminate\Contracts\Support\Arrayable<TKey, TValue>|iterable<TKey, TValue>|null  $value
-     * @param string|array $type
+     * @param \Illuminate\Contracts\Support\Arrayable<TKey, TValue>|iterable<TKey, TValue>|null $value
+     * @param string|string[] $types
      * @return \Illuminate\Support\Collection<TKey, TValue>
      */
     function typedCollect($value = [], string|array $types = [])
     {
         $types = is_array($types) ? $types : [$types];
 
-        $collection = new class extends TypedCollection
-        {
-        };
+        $collection = new class extends TypedCollection {};
 
         $reflectionClass = new ReflectionClass($collection);
         $reflectionProperty = $reflectionClass->getProperty('allowedTypes');
